5515ec05252e29e3a9e0f6cecb2090bf19e84fb2
nhck <n@nhck.de>
2019-08-12 17:37:18 +0000

    Splitter 6. Refractor to be universally usable

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -9,15 +9,11 @@ import './Stoppable.sol';
 contract Splitter is Stoppable {
     using SafeMath for uint;
 
-    event LogCustomerSet(address indexed sender, address indexed newCustomer, bool isSecond);
     event LogFundsSplit(address indexed sender, uint valuePerCustomer, address indexed firstCustomer, address indexed secondCustomer);
     event LogFundsWithdrawn(address indexed sender, uint value);
-
+    event LogBalanceUpdated(address indexed customer, uint newBalance);
 
     mapping(address => uint256) public balances;
-
-    address public firstCustomer;
-    address public secondCustomer;
 
     /**
 	 *
@@ -26,45 +22,25 @@ contract Splitter is Stoppable {
     constructor(bool startRunning)  Stoppable(startRunning) public {}
 
     /**
-     * Add/Update
-     *
-     * @param _newCustomer new Customer address
-     * @param isSecond Select customer first customer by 0 and second customer by 1
-     * @return sucess
-     */
-    function setCustomer(address payable _newCustomer, bool isSecond) onlyOwner onlyIfRunning public returns (bool success) {
-        require(_newCustomer != address(0));
-
-        if (!isSecond) {
-            firstCustomer = _newCustomer;
-            emit LogCustomerSet(msg.sender, _newCustomer, isSecond);
-        } else {
-            secondCustomer = _newCustomer;
-            emit LogCustomerSet(msg.sender, _newCustomer, isSecond);
-        }
-
-        return true;
-    }
-
-    /**
      * Assign half of the localbalance to the Customers
      * If the localbalance doesn't split even it is reduced until it splits even
      * Update the localbalance.
      *
      * @return true on success
      */
-    function split() onlyOwner onlyIfRunning public payable returns (bool success) {
-        //Test if splitters are actually set
+    function split(address firstCustomer, address secondCustomer) onlyIfRunning public payable returns (bool success) {
         require(firstCustomer != address(0) && secondCustomer != address(0), "Both customers have to be set.");
         require(msg.value > 0);
-        require(msg.value.mod(2) == 0, "Ammount not splittable.");
+        require(msg.value.mod(2) == 0, "Amount not splittable.");
 
         balances[firstCustomer] = balances[firstCustomer].add(msg.value.div(2));
         balances[secondCustomer] = balances[secondCustomer].add(msg.value.div(2));
 
+        emit LogBalanceUpdated(firstCustomer,balances[firstCustomer]);
+        emit LogBalanceUpdated(secondCustomer,balances[secondCustomer]);
         emit LogFundsSplit(msg.sender, msg.value.div(2), firstCustomer, secondCustomer);
 
-	return true;
+        return true;
     }
 
 
@@ -74,12 +50,12 @@ contract Splitter is Stoppable {
      * @return true on success
      */
     function withdrawFunds() onlyIfRunning public returns (bool success){
-        uint _ammount = balances[msg.sender];
-        require(_ammount > 0);
+        uint _amount = balances[msg.sender];
+        require(_amount > 0);
 
         balances[msg.sender] = 0;
-        emit LogFundsWithdrawn(msg.sender, _ammount);
-        msg.sender.transfer(_ammount);
+        emit LogFundsWithdrawn(msg.sender, _amount);
+        msg.sender.transfer(_amount);
 
         return true;
     }
--- test/splitter.js
+++ test/splitter.js
@@ -13,78 +13,53 @@ contract('Splitter Contract', accounts => {
         splitter = await SplitterFactory.new(true, {from: owner});
     });
 
-        it("should have an owner", async function () {
-            assert.equal(await splitter.getOwner(), owner);
-        });
-
-        it("should be able to set customers", async function () {
-
-            await splitter.setCustomer(first, false, {from: owner});
-            await splitter.setCustomer(first, true, {from: owner});
-
-            assert.equal(await splitter.firstCustomer(), first);
-            assert.equal(await splitter.secondCustomer(), first);
-
-        });
-
-        it("should fail on an malformed addresses", async function () {
-            let _malade = "0x01111";
-            await truffleAssert.fails(splitter.setCustomer(_malade, false, {from: owner}));
-        });
+    it("should have an owner", async function () {
+        assert.equal(await splitter.getOwner(), owner);
+    });
 
     describe("testing the splitting", function () {
-        let firstCustomerResult;
-        let secondCustomerResult;
 
-        beforeEach("setting up customers", async function () {
-            firstCustomerResult = await splitter.setCustomer(first, false, {from: owner});
-            secondCustomerResult = await splitter.setCustomer(second, true, {from: owner});
+        it("should not split uneven values", async function () {
+            let _amount = 5;
+
+            await truffleAssert.reverts(
+                splitter.split(first, second, {from: owner, value: _amount}),
+                "Amount not splittable."
+            );
+        });
+
+        it("should split even values", async function () {
+            let _amount = new BN(10);
+            let _halfamount = new BN(5);
+
+            let splitResult = await splitter.split(first, second, {from: owner, value: _amount});
+            let splitFirstBalance = await splitter.balances.call(first);
+            let splitSecondBalance = await splitter.balances.call(second);
+
+            assert.isTrue(_amount.eq(new BN(await web3.eth.getBalance(splitter.address))));
+            assert.isTrue(_halfamount.eq(splitFirstBalance));
+            assert.isTrue(_halfamount.eq(splitFirstBalance));
+
+            await truffleAssert.eventEmitted(splitResult, "LogFundsSplit", (ev) => {
+                return ev.sender === owner && _halfamount.eq(ev.valuePerCustomer) && ev.firstCustomer === first && ev.secondCustomer === second;
+            });
+            await truffleAssert.eventEmitted(splitResult, "LogBalanceUpdated", (ev) => {
+                return ev.customer === first && ev.newBalance.eq(splitFirstBalance);
+            });
+            await truffleAssert.eventEmitted(splitResult, "LogBalanceUpdated", (ev) => {
+                return ev.customer === second && ev.newBalance.eq(splitSecondBalance)
+            })
 
         });
 
-                it("should emit events on Customer setting", async function () {
-                    await truffleAssert.eventEmitted(firstCustomerResult, "LogCustomerSet", (ev) => {
-                        return ev.sender === owner && ev.newCustomer === first && ev.isSecond === false;
-                    });
-                    await truffleAssert.eventEmitted(secondCustomerResult, "LogCustomerSet", (ev) => {
-                        return ev.sender === owner && ev.newCustomer === second && ev.isSecond === true;
-                    });
-                });
-
-                it("should not split uneven values", async function () {
-                    let _ammount = 5;
-
-
-                    await truffleAssert.reverts(
-                        splitter.split({from: owner, value: _ammount}),
-                        "Ammount not splittable."
-                    );
-                });
-
-                it("should split even values", async function () {
-                    let _ammount = 10;
-                    let _halfammount = 5;
-
-                    let splitResult = await splitter.split({from: owner, value: _ammount});
-
-                    assert.equal(await web3.eth.getBalance(splitter.address), _ammount);
-                    assert.equal(await splitter.balances.call(first), _halfammount);
-                    assert.equal(await splitter.balances.call(first), _halfammount);
-
-                    await truffleAssert.eventEmitted(splitResult, "LogFundsSplit", (ev) => {
-                        return ev.sender === owner && ev.valuePerCustomer.toNumber() === _halfammount && ev.firstCustomer === first && ev.secondCustomer === second;
-                    });
-
-                });
-
         it("should allow withdrawal", async function () {
-            let _ammount = new BN(10);
-            let _halfammount = new BN(5);
+            let _amount = new BN(10);
+            let _halfamount = new BN(5);
 
             let firstAddressBalanceBefore = new BN(await web3.eth.getBalance(first));
 
 
-            await splitter.split({from: owner, value: _ammount});
+            await splitter.split(first, second, {from: owner, value: _amount});
 
             let _gasPrice = new BN(await web3.eth.getGasPrice());
             let withdrawResult = await splitter.withdrawFunds({from: first, gasPrice: _gasPrice.toNumber()});
@@ -94,26 +69,26 @@ contract('Splitter Contract', accounts => {
             let secondBalance = await splitter.balances.call(second);
 
             await truffleAssert.eventEmitted(withdrawResult, "LogFundsWithdrawn", (ev) => {
-                return ev.sender === first && (_halfammount).eq(ev.value);
+                return ev.sender === first && (_halfamount).eq(ev.value);
             });
 
             assert.isTrue(firstBalance.isZero()); //balance of first customer should be 0
-            assert.isTrue(secondBalance.eq(_halfammount)); //second customer should keep his half ammount
-            assert.isTrue(new BN(await web3.eth.getBalance(splitter.address)).eq(_halfammount)); //splitter contract should only have half
+            assert.isTrue(secondBalance.eq(_halfamount)); //second customer should keep his half ammount
+            assert.isTrue(new BN(await web3.eth.getBalance(splitter.address)).eq(_halfamount)); //splitter contract should only have half
 
 
             let transactionFee = new BN(withdrawResult.receipt.gasUsed);
             transactionFee = transactionFee.mul(_gasPrice);
             let firstAddressBalanceAfter = new BN(await web3.eth.getBalance(first));
 
-            assert.isTrue(firstAddressBalanceBefore.add(_halfammount).sub(transactionFee).eq(firstAddressBalanceAfter));
+            assert.isTrue(firstAddressBalanceBefore.add(_halfamount).sub(transactionFee).eq(firstAddressBalanceAfter));
 
         });
     });
 
-        it("should allow withdrawal only if you have a split balance", async function () {
-            await truffleAssert.reverts(
-                splitter.withdrawFunds({from: third})
-            )
-        });
+    it("should allow withdrawal only if you have a split balance", async function () {
+        await truffleAssert.reverts(
+            splitter.withdrawFunds({from: third})
+        )
+    });
 });
